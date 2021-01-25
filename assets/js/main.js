15d6afc3aa23e0910e0158bad5ea17b307aab306
duchuyta <d.huyta@gmail.com>
2021-01-25 22:12:26 +0700

    Optimize

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,7 +197,7 @@
       animation: 'fade', // Fade, slide, none
       animationInSpeed: 200, // Animation in speed (ms)
       animationOutSpeed: 200, // Animation out speed (ms)
-      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+      scrollText: '<svg width="20" height="20" viewBox="0 0 1792 1792"><path d="M1675 971q0 51-37 90l-75 75q-38 38-91 38-54 0-90-38l-294-293v704q0 52-37.5 84.5t-90.5 32.5h-128q-53 0-90.5-32.5t-37.5-84.5v-704l-294 293q-36 38-90 38t-90-38l-75-75q-38-38-38-90 0-53 38-91l651-651q35-37 90-37 54 0 91 37l651 651q37 39 37 91z" fill="#fff"/></svg>', // Text for element
       activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
 
@@ -209,7 +209,7 @@
 
 
 /* 10. WOW active */
-    new WOW().init();
+    // ew WOW().init();
 
 /* 11. Datepicker */
     
@@ -217,7 +217,7 @@
     function mailChimp() {
       $('#mc_embed_signup').find('form').ajaxChimp();
     }
-    mailChimp();
+    // mailChimp();
 
 
 // 12 Pop Up Img
@@ -232,10 +232,10 @@
       }
       
 /* 14. counterUp*/
-    $('.counter').counterUp({
-      delay: 10,
-      time: 3000
-    });
+    //$('.counter').counterUp({
+    //  delay: 10,
+    //  time: 3000
+    //});
 
   //Another popUp     
         var popUp = $('.menorie-icon');
